42bf5362ac1b306d6c422615617a31d9c916ecf5
EC2 Default User <ec2-user@ip-172-31-54-43.ec2.internal>
2022-03-01 15:28:27 +0000

    lab5

---
+++ lab5.py
@@ -0,0 +1,53 @@
+'''
+lab5 if statement
+'''
+
+#3.1
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('you earned 5 points')
+    
+#3.2
+
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print('you earned 5 points')
+    
+else: 
+    print('you just earned 10 points')
+    
+#3.3 
+
+favorite_fruits = ['grapes', 'banana', 'orange']
+if 'orange' in favorite_fruits:
+    print('you really like orange')
+    
+if 'grapes' in favorite_fruits:
+    print('you really like grape')
+    
+if 'mango' in favorite_fruits:
+    print('you really like mango')
+    
+#3.4 
+
+age = 67
+
+#if age < 10:
+ #   print('you are a kid')
+#elif age <20:
+#    print('you are a teenager')
+#else:
+ #   print('you are an adult')
+ #   if age >65:
+ #       print('you are an elder')
+
+if age >=20:
+    print('you are an adult')
+    if age >65:
+        print('you are an elder')
+elif age >10:
+    print('you are a teenager')
+else: 
+    print('you are a kid')
